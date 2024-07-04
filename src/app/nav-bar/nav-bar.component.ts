a1b2af5bcddfde26d9e086d213716bc4556b01e7
zee-creato <zbigniew.kierpiec@proav.com>
2024-07-04 10:40:41 +0100

    added

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink , NgClass],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+
+
+  isSticky: boolean = false;
+  navbarHeight: number = 0;
+  pageTopOffset: number = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+      const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+      // Calculate the bottom of the navbar relative to the viewport
+      const navbarBottom = this.pageTopOffset + this.navbarHeight;
+
+      if (scrollOffset >= navbarBottom) {
+          this.isSticky = true;
+      } else {
+          this.isSticky = false;
+      }
+  }
+
+  ngOnInit(): void {
+      this.navbarHeight = document.getElementById('navbar')!.offsetHeight;
+      this.pageTopOffset = document.getElementById('navbar')!.offsetTop;
+  }
+
+
 
 }
